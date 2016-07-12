fe017d5d73a81aa457c038ab174cae987a9aab41
Josh T <jptucker2009@gmail.com>
2016-07-12 15:26:30 -0400

    Dom explorer added

---
+++ src/writeTable.java
@@ -0,0 +1,14 @@
+
+public class writeTable {
+	public writeTable(){
+		
+	}
+	public String createTableHtml(){
+		String htmlT=" ";
+		String Tags ="<table>"+"</table>"+"<tr>"+"</tr>"+"<td>"+"</td>";
+		
+		
+		return null;
+		
+	}
+}
